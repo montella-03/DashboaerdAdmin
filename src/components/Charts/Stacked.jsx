fd5e4f38c99716c70beafc0139f8b706e81795a2
christopher <oigoc60@gmail.com>
2022-12-07 20:19:17 +0300

    ok

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,39 +1,31 @@
-import React from "react";
-import {
-  ChartComponent,
-  SeriesCollectionDirective,
-  SeriesDirective,
-  Inject,
-  Legend,
-  Category,
-  StackingColumnSeries,
-  Tooltip,
-} from "@syncfusion/ej2-react-charts";
-import {
-  stackedCustomSeries,
-  stackedPrimaryXAxis,
-  stackedPrimaryYAxis,
-} from "../../data/dummy";
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
 
-function Stacked({width,height}) {
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+
   return (
     <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
       width={width}
       height={height}
-      id='stack chart'
-      primarryXAxis={stackedPrimaryXAxis}
-      primaryYAxis={stackedPrimaryYAxis}
       chartArea={{ border: { width: 0 } }}
-      tooltip={{enable:true}}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      legendSettings={{ background: 'white' }}
     >
-      
-      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
       <SeriesCollectionDirective>
-        {stackedCustomSeries.map((item, index) =>
-          <SeriesDirective key={index} {...item} />)}
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
       </SeriesCollectionDirective>
     </ChartComponent>
   );
-}
+};
 
 export default Stacked;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,45 @@
-import React, { useEffect } from "react";
-import { AiOutlineMenu } from "react-icons/ai";
-import { FiShoppingCart } from "react-icons/fi";
-import { BsChatLeft } from "react-icons/bs";
-import { RiNotification3Line } from "react-icons/ri";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import avatar from "../data/avatar.jpg";
-import { Cart, Chat, Notification, UserProfile } from "./Index";
-import { useStateContext } from "../contexts/ContextProvider";
+import React, { useEffect } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-const NavButton = ({ title, customFunc, icon, Color, dotColor }) => (
-  <TooltipComponent context={title} position="BottomCenter">
+import avatar from '../data/avatar.jpg';
+import { Cart, Chat, Notification, UserProfile } from '.';
+import { useStateContext } from '../contexts/ContextProvider';
+
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="BottomCenter">
     <button
       type="button"
-      onClick={customFunc}
-      className="relative text-xl rounded-full p-4 hover:bg-light-gray"
+      onClick={() => customFunc()}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
     >
       <span
         style={{ background: dotColor }}
-        className="absolute inline-flex rounded-full h-2 w-2 left--1 top-1"
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
       />
       {icon}
     </button>
   </TooltipComponent>
 );
 
-function Navbar() {
-  const {
-    activeMenu,
-    setActiveMenu,
-    isClicked,
-    setisClicked,
-    handleClick,
-    screenSize,
-    setscreenSize,
-  } = useStateContext();
-  useEffect(() => {
-    const handleResize = () => setscreenSize(window.innerWidth);
-    window.addEventListener("resize", handleResize);
+const Navbar = () => {
+  const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-      handleResize();
-    };
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
+
   useEffect(() => {
     if (screenSize <= 900) {
       setActiveMenu(false);
@@ -52,60 +48,43 @@ function Navbar() {
     }
   }, [screenSize]);
 
+  const handleActiveMenu = () => setActiveMenu(!activeMenu);
+
   return (
-    <div className="flex justify-between p-2 md:mx-6 relative">
-      <NavButton
-        title="Menu"
-        customFunc={() => setActiveMenu((prevActionMenu) => !prevActionMenu)}
-        Color="blue"
-        icon={<AiOutlineMenu />}
-      />
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
 
+      <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
       <div className="flex">
-        <NavButton
-          title="Cart"
-          customFunc={() => handleClick("cart")}
-          Color="blue"
-          icon={<FiShoppingCart />}
-          dotColor="black"
-        />
-        <NavButton
-          title="Chat"
-          customFunc={() => handleClick("chat")}
-          dotColor="green"
-          Color="blue"
-          icon={<BsChatLeft />}
-        />
-        <NavButton
-          title="Notification"
-          customFunc={() => handleClick("notification")}
-          dotColor="red"
-          Color="blue"
-          icon={<RiNotification3Line />}
-        />
-
-        <TooltipComponent content="profile" position="BottomCenter">
+        <NavButton title="Cart" customFunc={() => handleClick('cart')} color={currentColor} icon={<FiShoppingCart />} />
+        <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => handleClick('chat')} color={currentColor} icon={<BsChatLeft />} />
+        <NavButton title="Notification" dotColor="rgb(254, 201, 15)" customFunc={() => handleClick('notification')} color={currentColor} icon={<RiNotification3Line />} />
+        <TooltipComponent content="Profile" position="BottomCenter">
           <div
-            className="flex items-center gap-2 p-1 hover:bg-gray-50 cursor-pointer"
-            onClick={() => handleClick("userProfile")}
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick('userProfile')}
           >
-            <img className="rounded-full w-8 h-8" src={avatar} />
+            <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt="user-profile"
+            />
             <p>
-              <span className="text-gray-400 text-14">Hi,</span>{" "}
-              <span className="text-gray-400 text-14 font-bold ml-1">
-                Montella
+              <span className="text-gray-400 text-14">Hi,</span>{' '}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
               </span>
             </p>
-            <MdKeyboardArrowDown className="text-14 text-gray-500" />
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
         </TooltipComponent>
-        {isClicked.cart && <Cart />}
-        {isClicked.chat && <Chat />}
-        {isClicked.notification && <Notification />}
-        {isClicked.userProfile && <UserProfile />}
+
+        {isClicked.cart && (<Cart />)}
+        {isClicked.chat && (<Chat />)}
+        {isClicked.notification && (<Notification />)}
+        {isClicked.userProfile && (<UserProfile />)}
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
